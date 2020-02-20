2bfff047dbe66cd77fe71d850f6625fd4bb9474c
Jia <49358172+craftslab@users.noreply.github.com>
2020-02-20 15:46:57 +0800

    Update run.sh

--- script/run.sh
+++ script/run.sh
@@ -11,5 +11,5 @@
 # curl https://api.github.com/search/code?q=runSearch+in:file+language:go+repo:githubsearch+user:craftslab&page=1&per_page=10&sort=stars&order=desc
 # curl https://api.github.com/search/repositories?q=githubsearch+language:go+user:craftslab&page=1&per_page=10&sort=stars&order=desc
 
-go run main.go --api "rest" --config "config/search.json" --output "output.json" --qualifier "in:file,language:go,repo:githubsearch,user:craftslab" --search "code:runSearch"
-go run main.go --api "rest" --config "config/search.json" --output "output.json" --qualifier "language:go+user:craftslab" --search "repo:githubsearch"
+go run main.go --api="rest" --config="config/search.json" --output="output.json" --qualifier="in:file,language:go,repo:githubsearch,user:craftslab" --search="code:runSearch"
+go run main.go --api="rest" --config="config/search.json" --output="output.json" --qualifier="language:go+user:craftslab" --search="repo:githubsearch"
